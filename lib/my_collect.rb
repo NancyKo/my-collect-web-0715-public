4db31b62a8725ea9fd8a0dcdedc0dcb408f052c9
NancyKo <konancys@gmail.com>
2015-08-01 22:11:39 -0400

    Finished with yield

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,24 @@
+require "pry"
 def my_collect(array)
   puts "This block should not run!" if array.empty?
-  if array.count != 0
-    array.collect {|e| e.upcase}
+  new_collection = []
+  i = 0
+  while array.size > i
+    new_collection.push(yield array[i].upcase)
+    i += 1
   end
+  new_collection
 end
+
+
+    
+
+# my_collect( ['ruby', 'javascript', 'python', 'objective-c'] )
+
+
+# def my_collect(array)
+#   puts "This block should not run!" if array.empty?
+#   if array.count != 0
+#     array.collect {|e| e.upcase}
+#   end
+# end
